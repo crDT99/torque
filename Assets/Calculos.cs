592f7f73cab2dd42ca4f708b6f48fcf4eaa3baf2
crDTbD <61856236+crDTbD@users.noreply.github.com>
2020-06-04 07:15:11 -0500

    Assets folder

---
+++ Assets/Calculos.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Calculos : MonoBehaviour
+{
+    Quaternion quaternion;
+    public GameObject Obj_2;
+    public float radio, masa, Momento_de_Inercia, t;
+    public  Vector3 posA_actual, posA_Sig, fTorque, Acel_ang, Vel_ang;
+
+
+    public InputField aceleracion, torque ;
+
+    void Start()
+    {   
+        Vector3 tmp = Obj_2.GetComponent<Transform>().position;
+        radio = tmp.x;
+        masa = 0.5f;
+        Vel_ang.x = 0;
+        Vel_ang.y = 0;
+        Vel_ang.z = 0;
+        t = 0.01f;
+       
+        
+    }
+
+
+    void Update()
+    {
+
+        Vector3 tmp = Obj_2.GetComponent<Transform>().position;
+        radio = tmp.x;
+        posA_actual = gameObject.GetComponent<Transform>().rotation.eulerAngles;   
+        fTorque = Obj_2.GetComponent<mouse>().fAplicada * radio;
+        Momento_de_Inercia = (masa * Mathf.Pow(radio, 2)) / 3;
+        Acel_ang = fTorque / Momento_de_Inercia;
+        Vel_ang = Vel_ang + Acel_ang * t;
+        posA_actual = posA_actual + Vel_ang * t; posA_Sig = posA_actual;
+        quaternion = Quaternion.Euler(posA_actual.x, posA_actual.y, posA_actual.z);
+        gameObject.GetComponent<Transform>().rotation = quaternion;
+        aceleracion.text = Acel_ang.y.ToString(); torque.text = fTorque.y.ToString();
+    }
+}
---
+++ Assets/mouse.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mouse : MonoBehaviour
+{   
+    public InputField Fuerza;
+   
+    public Vector3 fAplicada;
+    float mFuerza;
+ 
+ Vector3 pos_base, pos_click, pos_click_sig, pos_Mouse, VectorF, Ang;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        pos_base = transform.position;
+    }
+
+    // Update is called once per frame
+    void OnMouseDrag()
+    {
+
+        pos_Mouse = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 20);
+
+        pos_click = Camera.main.ScreenToWorldPoint(pos_Mouse);
+
+        transform.position = pos_click;
+    }
+
+    void OnMouseUp()
+    {
+
+        VectorF.x = Mathf.Abs(pos_click.x - pos_base.x);
+        VectorF.y = Mathf.Abs(pos_click.y - pos_base.y);
+        VectorF.z = Mathf.Abs(pos_click.z - pos_base.z);
+        mFuerza = VectorF.magnitude;
+        Ang = VectorF.normalized;
+        fAplicada.y = (mFuerza * Ang.x);
+        if(pos_click.x<0 && pos_click.z>pos_click_sig.z)
+        {
+            fAplicada.y = -fAplicada.y;
+        }
+        else if (pos_click.x > 0 && pos_click.z >pos_click_sig.z)
+        {
+            fAplicada.y = -fAplicada.y;
+        }
+         
+        Fuerza.text  = fAplicada.y.ToString();
+        pos_click_sig = pos_click;
+    }
+}
+
+
+
